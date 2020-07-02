57ee6a6fc0982ee8d8cf1d71be611da90ac92ea4
김용진 <66247799+yonggar@users.noreply.github.com>
2020-07-02 17:48:30 +0900

    last

--- baekjoon.py
+++ baekjoon.py
@@ -2,9 +2,15 @@ T=int(input())
 
 while T!=0:
     T-=1
-    X=[]
     k=int(input())
     n=int(input())
-    x0=[i for i in range(1,n+1)]
-    
-    
+    X=[[] for _ in range(k+1)]
+    X[0]=[i for i in range(n+1)]
+    for i in range(1,k+1):
+        a=[]
+        for j in range(n+1):
+            a.append(X[i][j]+X[i+1][j-1])
+        X[i]=a
+
+    print(X)
+
