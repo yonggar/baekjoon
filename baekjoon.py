a60ce1592ac9191cac671a35b3ed6870d10ab2d6
김용진 <66247799+yonggar@users.noreply.github.com>
2020-07-07 15:31:36 +0900

    1929

--- baekjoon.py
+++ baekjoon.py
@@ -1,25 +1,19 @@
-import math
-T=int(input())
+M=int(input())
+N=int(input())
 
-while T!=0:
-    T-=1
-    x,y=map(int,input().split())
-    s=y-x
-    #s의 제곱근구하기
-    sr=math.ceil(s**(1/2))
-    #홀수수열값
-    odd=2*sr-1
-    if s**(1/2)==sr:
-        print(odd)
+d=[]
+for i in range(M,N+1):
+    if i==2:
+        d.append(i)
     else:
-        if s>sr**2-sr:
-            print(odd)
-        else:
-            print(odd-1)
+        for j in range(2,i):
+            if i%j==0:
+                break
+            elif i==j+1:
+                d.append(i)
 
-a=1
-    ###s가 제곱근(1**2,2**2,3**2,4**2~)일때 (1,3,5,7,9~)수열을이룬다
-    ###   제곱근 해당 수 만큼 처리과정이잇고 그앞은 -1
-    ###cf) 49~56(14) 57~64(15) 65~72(16) 73~81(17) 82~90(18) 91~100(19)
-
-    #제너레이터로만들어 메모리줄이기 or 마지막값으로만계산출력
+if d==[]:
+    print(-1)
+else:
+    print(sum(d))
+    print(d[0])
